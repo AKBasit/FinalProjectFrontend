138555036a8a1853fedd4db2e3420b5720b2bb28
Ian IJmkers <ianymkers@gmail.com>
2024-01-25 18:39:08 +0100

    Read and CRUD forms

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -3,19 +3,27 @@ import { UserContext } from "../contexts/UserContext";
 import { Link } from "react-router-dom";
 import ProfileHeader from "./ProfileComponents/ProfileHeader";
 import WebDesignList from "./WebDesignList";
-import CreateGeneral from "./CreateGeneral";
 
 export default function Profile() {
   const { user, handleLogout } = useContext(UserContext);
   return (
     <div>
       <ProfileHeader />
+      <div className="p-20">
+        <Link className="px-4" to="/createWebdesigns">
+          Create Web Design
+        </Link>
+        <Link className="px-4" to="/createFonts">
+          Create Font
+        </Link>
+        <Link className="px-4" to="/createImages">
+          Create Image
+        </Link>
+      </div>
       <WebDesignList />
-      <Link to="/createWebdesigns">Create Web Design</Link>
-      <Link to="/createFonts">Create Font</Link>
-      <Link to="/createImages">Create Image</Link>
-      <CreateGeneral />
-      <button onClick={handleLogout}>Logout</button>
+      <button onClick={handleLogout} className="p-20">
+        Logout
+      </button>
     </div>
   );
 }
