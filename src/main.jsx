0560a7a8ce685b0869ba72cb13ac89b574351b2d
Ian IJmkers <ianymkers@gmail.com>
2024-02-08 20:01:26 +0100

    finished?

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { BrowserRouter, Link } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { UserContextWrapper } from "./contexts/UserContext.jsx";
 import App from "./App.jsx";
 import { WebDesignWrapper } from "./contexts/WebDesignContext.jsx";
@@ -11,12 +11,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <BrowserRouter>
       <UserContextWrapper>
         <WebDesignWrapper>
-          <App>
-            <div>
-              <Link to="/">First</Link>
-              <Link to="/second">Second</Link>
-            </div>
-          </App>
+          <App />
         </WebDesignWrapper>
       </UserContextWrapper>
     </BrowserRouter>
